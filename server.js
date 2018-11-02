9aa72ddb74c35a2d5a55ac490d22d6b4217776ce
Hermit <zhong76@purdue.edu>
2018-11-02 05:27:52 -0400

    Add parsing

--- server.js
+++ server.js
@@ -53,4 +53,4 @@ http.createServer(function (req, res) {
 
 
   res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080
+}).listen(8000); //the server object listens on port 8080
