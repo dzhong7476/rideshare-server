f382f4bdcbbe965457d7c1fb35e69bf4c7e4063e
Hermit <zhong76@purdue.edu>
2018-11-02 05:20:48 -0400

    Add parsing

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var nodemailer = require('nodemailer');
-
+var http = require('http');
 var transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
@@ -12,20 +12,45 @@ var mailOptions = {
   from: 'rideshareofficial.com',
   to: 'zhongdai.sw@gmail.com',
   subject: 'Sending Email using Node.js',
-  text: 'That was easy!'
+  text: ''
 };
-var http = require('http');
+function parsing(msg) {
+  var parts = msg.split('\n');
+  var paymentMethods = parts[6].split(",");
+  var styledpaymentmethods = '';
+  for (var i = 0; i < paymentMethods.length; i++) {
+    styledpaymentmethods += paymentMethods[i];
+    styledpaymentmethods += " ";
+  }
+  mailOptions.to = parts[0];
+  mailOptions.subject = parts[1];
+  mailOptions.text = "Date: " + parts[2] + "\n" +
+  "Time: " + parts[3] + "\n" +
+  "Start Location: " + parts[4] + "\n" +
+  "Destination: " + parts[5] + "\n" +
+  "PaymentMethods: " + styledpaymentmethods + "\n" +
+  "Cost: " + parts[7] + "\n" +
+  "Description: " + parts[8] + "\n";
+}
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Hello World!'); //write a response to the client
-  transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Email sent: ' + info.response);
-    }
+  let body = [];
+  req.on('data', (chunk) => {
+    body.push(chunk);
+  }).on('end', () => {
+    body = Buffer.concat(body).toString();
+    parsing(body)
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
+    // at this point, `body` has the entire request body stored in it as a string
   });
+
 
   res.end(); //end the response
 }).listen(8080); //the server object listens on port 8080
